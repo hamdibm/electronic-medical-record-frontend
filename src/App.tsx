8cc9684e5cf99b4552ebbc39e4c37266e92b57f3
Aziz <Azizbelkhir93@gmail.com>
2025-03-13 10:46:02 +0100

    creation de l'interface Register

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
+import React from 'react';
+import Login from './components/LoginRegistre/Login'; // Chemin relatif correct
+import './App.css'; // Importez le fichier CSS si nécessaire
 
 function App() {
- 
   return (
-    <>
-      <div className="App">
-        <h1>React App</h1>
-      </div>
-    </>
-  )
+    <div>
+      <h1>Mon Application</h1>
+      <Login /> {/* Utilisation du composant Login */}
+    </div>
+  );
 }
 
-export default App
+export default App;
