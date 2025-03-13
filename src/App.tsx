5bd3ad8fb603bfaa74a73a9bc6a8fd6f37681c14
Aziz <Azizbelkhir93@gmail.com>
2025-03-13 14:05:54 +0100

    modification avec Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import Login from './components/LoginRegistre/Login'; // Chemin relatif correct
-import './App.css'; // Importez le fichier CSS si nécessaire
+import Login from "./components/LoginRegistre/Login.jsx" // Chemin relatif correct
+import './App.css'; // Importez le fichier CSS une seule fois
 
 function App() {
   return (
     <div>
-      <h1>Mon Application</h1>
+      <h1 className="text-center">Mon Application</h1>
       <Login /> {/* Utilisation du composant Login */}
     </div>
   );
